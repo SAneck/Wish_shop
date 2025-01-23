55f5c956f57fd5c830941ccc40ed355784d27c6a
Усатов Александр <a.usatov@tepts.local>
2025-01-23 16:36:58 +0300

    Сделал слайдер

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import classes from './App.module.scss'
 import { Header } from './components/header/header'
 import { ProductCard } from './components/productCard/ProductCard'
 import { productCard } from './components/productCard/ProductCardContent'
+import { Slider } from './components/slider/Slider'
+
 export const App = () => {
 	return (
 		<div className={classes.container}>
 			<Header />
+			<Slider />
+			<h1>Каталог</h1>
 			<div className={classes.wrapper}>
 				{Array.from({length: 12}, (_, index) =>(
 					<ProductCard key={index} card={productCard}/>
 				))}
+
 			</div>
 		</div>
 	)
