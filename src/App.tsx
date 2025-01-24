882291eb5b56e1d2eab35d1e59fc9a7bb616bb6a
Усатов Александр <a.usatov@tepts.local>
2025-01-24 16:18:28 +0300

    restore govno

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import classes from './App.module.scss'
 import { Header } from './components/header/header'
-import { ProductCard } from './components/ProductCard/ProductCard'
-import { productCard } from './components/ProductCard/ProductCard_content'
+import { ProductCard } from './components/productCard/ProductCard'
+import { productCard } from './components/productCard/ProductCardContent'
+import { SideBar } from './components/sidebar/SideBar'
+import { Slider } from './components/slider/Slider'
 
 export const App = () => {
 	return (
 		<div className={classes.wrapper}>
 			<Header />
-
-			{Array.from({ length: 10 }, (_, index) => (
-				<ProductCard key={index} card={productCard} />
-			))}
+			<Slider />
+			<h1>Каталог</h1>
+			<div className={classes.wrapper}>
+				<div className={classes.sideBar}>
+					<SideBar />
+				</div>
+				<div className={classes.cards}>
+					{Array.from({ length: 12 }, (_, index) => (
+						<ProductCard key={index} card={productCard} />
+					))}
+				</div>
+			</div>
 		</div>
 	)
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,21 +1,28 @@
-import classes_header from './Header.module.scss'
-
+import classes from './Header.module.scss'
 export const Header = () => {
 	return (
-		<div className={classes_header.header}>
-			<p className={classes_header.logo}>Wish</p>
-			<ul>
-				<li>
-					<a href='#'>Каталог</a>
-				</li>
-				<li>
-					<a href='#'>Поддержка</a>
-				</li>
-				<li>
-					<a href='#'>Корзина</a>
-				</li>
-			</ul>
-			<a href='#'>Вход</a>
-		</div>
+		<>
+			<div className={classes.header}>
+				<a href='#' className={classes.logo}>
+					Wish
+				</a>
+				<ul>
+					<li>
+						<a href='/'>Каталог</a>
+					</li>
+					<li>
+						<a href='/'>Поддержка</a>
+					</li>
+					<li>
+						<a href='../favorite/Favorite'>Избранное</a>
+					</li>
+					<li>
+						<a href='/'>Корзина</a>
+					</li>
+					<input type='text' placeholder='Искать'></input>
+				</ul>
+				<a href='#'>Вход</a>
+			</div>
+		</>
 	)
 }
